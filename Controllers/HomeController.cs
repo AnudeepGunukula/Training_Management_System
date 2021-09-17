b24629027e3104a9d245c64814718aa23630e571
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-09-17 09:56:43 +0530

    commit after login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Training_Management_System.Models;
 
 namespace Training_Management_System.Controllers
 {
@@ -26,5 +30,56 @@ namespace Training_Management_System.Controllers
 
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["TrainingDbEntities"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            string cmdtext = "checkUsers";
+            SqlCommand cmd = new SqlCommand(cmdtext, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+
+            cmd.Parameters.AddWithValue("username", user.UserName);
+            cmd.Parameters.AddWithValue("password", user.Password);
+
+
+
+            conn.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            if(sdr.HasRows)
+            {
+                while(sdr.Read())
+                {
+
+                    string temprole = sdr["role"].ToString();
+
+                    if (temprole == "Trainer")
+                    {
+                        return RedirectToAction("Index", "Trainer");
+                    }
+                    else if (temprole == "TrainingManager")
+                    {
+                        return RedirectToAction("Index", "TrainingManager");
+                    }
+                }
+               
+            }
+
+            return View();
+        }
+
+
     }
 }
